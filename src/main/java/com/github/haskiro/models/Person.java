9fac7dcbe23effd505a1b5f69efb7f2ce20fc072
Павел <58857483+Haskiro@users.noreply.github.com>
2023-03-12 16:50:58 +0300

    supporting russian language, PeopleController, Person model and PersonDAO were added. listing all existing people

---
+++ src/main/java/com/github/haskiro/models/Person.java
@@ -0,0 +1,46 @@
+package com.github.haskiro.models;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "ФИО не должно быть пустым")
+    @Size(min = 6, max = 100, message = "ФИО должно быть длинной в диапазоне от 6 до 100 символов")
+    private String fullName;
+    @Min(value = 1900, message = "Год рождения должен быть больше 1900")
+    private int yearOfBirth;
+
+    public Person() {}
+
+    public Person(int id, String fullName, int yearOfBirth) {
+        this.id = id;
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
